32332a48cb40fc2d6cb4d51dff5d5b37b958fa65
rubencharly94 <rubencharly94@hotmail.com>
2021-09-22 04:24:07 -0500

    v1
    initial structure creation

---
+++ src/algorithmsandconstructs/BookingSystem.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algorithmsandconstructs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+/**
+ *
+ * @author ruben
+ */
+public class BookingSystem implements BookingSystemInterface{
+
+    @Override
+    public RentACarInterface setupRentACar(BufferedReader in) throws IOException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
